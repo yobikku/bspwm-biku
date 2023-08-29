2b64e5c20e5fda4701e9f8515284874ca794d3f8
Jovic Gutierrez <jovicgutierrez@proton.me>
2023-08-29 20:48:34 +0800

    restructured for easy linking and copy

--- install.sh
+++ install.sh
@@ -5,3 +5,15 @@ if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
   exit 1
 fi
+
+pacman -S stow
+
+# Make Default Directories
+mkdir ~/Desktop
+mkdir ~/Documents
+mkdir ~/Downloads
+mkdir ~/Music
+mkdir ~/Pictures
+mkdir ~/Public
+mkdir ~/Templates
+mkdir ~/Videos
